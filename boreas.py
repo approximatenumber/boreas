14b61d39d5452df7af54f2cee1686ebacb404cd9
Melnikov <approximatenumber@gmail.com>
2021-04-27 22:54:17 +0300

    more regiters for controller

--- boreas.py
+++ boreas.py
@@ -10,7 +10,27 @@ wind_sensor = WindSensor()
 
 data_dispath = {
     'controller': {
-        'bat_voltage': controller.get_battery_voltage,
+        'accum_solar_gen_energy': controller.get_accum_solar_gen_energy,
+        'accum_wind_gen_energy': controller.get_accum_wind_gen_energy,
+        'battery_voltage': controller.get_battery_voltage,
+        'daily_consump_energy': controller.get_daily_consump_energy,
+        'daily_gen_energy': controller.get_daily_gen_energy,
+        'daily_solar_gen_energy': controller.get_daily_solar_gen_energy,
+        'daily_wind_gen_energy': controller.get_daily_wind_gen_energy,
+        'load_1_current': controller.get_load_1_current,
+        'load_1_power': controller.get_load_1_power,
+        'load_2_current': controller.get_load_2_current,
+        'load_2_power': controller.get_load_2_power,
+        'pv_current': controller.get_pv_current,
+        'pv_power': controller.get_pv_power,
+        'pv_voltage': controller.get_pv_voltage,
+        'state': controller.get_state,
+        'total_consump_energy': controller.get_total_consump_energy,
+        'total_gen_energy': controller.get_total_gen_energy,
+        'wind_current': controller.get_wind_current,
+        'wind_power': controller.get_wind_power,
+        'wind_rotation_speed': controller.get_wind_rotation_speed,
+        'wind_voltage': controller.get_wind_voltage
     },
     'wind_sensor': {
         'wind_power': wind_sensor.get_wind_power
--- lib/controller.py
+++ lib/controller.py
@@ -1,6 +1,6 @@
 import minimalmodbus
 
-from lib.devices import DeviceDefinition
+from lib.device_configurations import ControllerConfig
 
 
 class ModBusDevice():
@@ -17,9 +17,10 @@ class ModBusDevice():
     def read_register(self, register: int, decimals: int = 1) -> float:
         return self.conn.read_register(register, decimals, functioncode=3)
 
+
 class Controller(ModBusDevice):
 
-    def __init__(self, config=DeviceDefinition.Controller):
+    def __init__(self, config=ControllerConfig):
         super().__init__(config=config)
 
     def get_state(self):
@@ -39,3 +40,56 @@ class Controller(ModBusDevice):
 
     def get_wind_rotation_speed(self):
         return self.read_register(self.config.WIND_ROT_SPEED_REG)
+
+    def get_pv_voltage(self):
+        return self.read_register(self.config.PV_VOLTAGE)
+
+    def get_pv_current(self):
+        return self.read_register(self.config.PV_CURRENT)
+
+    def get_pv_power(self):
+        return self.read_register(self.config.PV_POWER)
+
+    def get_load_2_current(self):
+        return self.read_register(self.config.LOAD_2_CURRENT)
+
+    def get_load_1_current(self):
+        return self.read_register(self.config.LOAD_1_CURRENT)
+
+    def get_load_2_power(self):
+        return self.read_register(self.config.LOAD_2_POWER)
+
+    def get_load_1_power(self):
+        return self.read_register(self.config.LOAD_1_POWER)
+
+    def get_daily_wind_gen_energy(self):
+        return self.read_register(self.config.DAILY_WIND_GEN_ENERGY)
+
+    def get_accum_wind_gen_energy(self):
+        high = self.read_register(self.config.ACCUM_WIND_GEN_ENERGY_HIGH)
+        low = self.read_register(self.config.ACCUM_WIND_GEN_ENERGY_LOW)
+        return high+low
+
+    def get_daily_solar_gen_energy(self):
+        return self.read_register(self.config.DAILY_SOLAR_GEN_ENERGY)
+
+    def get_accum_solar_gen_energy(self):
+        high = self.read_register(self.config.ACCUM_SOLAR_GEN_ENERGY_HIGH)
+        low = self.read_register(self.config.ACCUM_SOLAR_GEN_ENERGY_LOW)
+        return high+low
+
+    def get_daily_gen_energy(self):
+        return self.read_register(self.config.DAILY_GEN_ENERGY)
+
+    def get_total_gen_energy(self):
+        high = self.read_register(self.config.TOTAL_GEN_ENERGY_HIGH)
+        low = self.read_register(self.config.TOTAL_GEN_ENERGY_LOW)
+        return high+low
+
+    def get_daily_consump_energy(self):
+        return self.read_register(self.config.DAILY_CONSUMP_ENERGY)
+
+    def get_total_consump_energy(self):
+        high = self.read_register(self.config.TOTAL_CONSUMP_ENERGY_HIGH)
+        low = self.read_register(self.config.TOTAL_CONSUMP_ENERGY_LOW)
+        return high+low
