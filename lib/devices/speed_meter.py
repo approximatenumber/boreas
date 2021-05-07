3a409c2d8671f0023f54b9414e2c4d32d75a8f39
Melnikov <approximatenumber@gmail.com>
2021-05-07 23:51:38 +0300

    add black boxes

--- lib/devices/speed_meter.py
+++ lib/devices/speed_meter.py
@@ -2,7 +2,7 @@ from lib.devices.black_box_meter_device import BlackBoxMeterDevice
 from lib.devices.device_configurations import SpeedMeterConfig
 
 
-class TorqueMeter(BlackBoxMeterDevice):
+class SpeedMeter(BlackBoxMeterDevice):
     def __init__(self):
         super().__init__(config=SpeedMeterConfig)
 
