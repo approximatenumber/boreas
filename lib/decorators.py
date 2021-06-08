48144412bebdbe4174236aedb6be065d91695271
Maksim Melnikov <maksim.melnikov@arrival.com>
2021-06-08 15:59:51 +0300

    added division decorators

--- lib/decorators.py
+++ lib/decorators.py
@@ -1,13 +1,13 @@
 def divide_by_10(func):
     """Divides function output value by 10."""
-    def wrapped(self, args, **kwargs):
+    def wrapped(self):
         value = func(self)
         return value / 10 if value else None
     return wrapped
 
 def divide_by_100(func):
     """Divides function output value by 100."""
-    def wrapped(self, args, **kwargs):
+    def wrapped(self):
         value = func(self)
         return value / 100 if value else None
     return wrapped
