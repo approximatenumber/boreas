2a1cb015abbd431d50a37a3df6dcbd734afa28e8
Maksim Melnikov <maksim.melnikov@arrival.com>
2021-06-08 17:44:30 +0300

    changes

--- lib/decorators.py
+++ lib/decorators.py
@@ -11,19 +11,19 @@ def divide_by_10(func):
     """Divides function output value by 10."""
     def wrapped(self):
         value = func(self)
-        return value / 10 if _is_number(str(value)) else None
+        return value / 10 if _is_number(str(value)) else value
     return wrapped
 
 def divide_by_100(func):
     """Divides function output value by 100."""
     def wrapped(self):
         value = func(self)
-        return value / 100 if _is_number(str(value)) else None
+        return value / 100 if _is_number(str(value)) else value
     return wrapped
 
 def multiply_by_10(func):
     """Mutiplies function output value by 10."""
     def wrapped(self):
         value = func(self)
-        return value * 10 if _is_number(str(value)) else None
+        return value * 10 if _is_number(str(value)) else value
     return wrapped
