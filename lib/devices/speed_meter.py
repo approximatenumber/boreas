e4512297157d2d44d0b984c62b92a7e882769050
Maksim Melnikov <maksim.melnikov@arrival.com>
2021-06-08 16:14:27 +0300

    fixes

--- lib/devices/speed_meter.py
+++ lib/devices/speed_meter.py
@@ -1,10 +1,12 @@
 from lib.devices.black_box_meter_device import BlackBoxMeterDevice
 from lib.devices.device_configurations import SpeedMeterConfig
+from lib.decorators import multiply_by_10
 
 
 class SpeedMeter(BlackBoxMeterDevice):
     def __init__(self):
         super().__init__(config=SpeedMeterConfig)
 
+    @multiply_by_10
     def get_speed(self):
         return self.write_value_and_read_answer(SpeedMeterConfig.SPEED_CMD)
--- lib/devices/torque_meter.py
+++ lib/devices/torque_meter.py
@@ -1,11 +1,9 @@
 from lib.devices.device_configurations import TorqueMeterConfig
 from lib.devices.black_box_meter_device import BlackBoxMeterDevice
-from lib.decorators import multiply_by_10
 
 class TorqueMeter(BlackBoxMeterDevice):
     def __init__(self):
         super().__init__(config=TorqueMeterConfig)
 
-    @multiply_by_10
     def get_peak(self):
         return self.write_value_and_read_answer(TorqueMeterConfig.PEAK_CMD)
