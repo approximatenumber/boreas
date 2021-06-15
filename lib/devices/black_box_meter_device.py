c73849aa533d74ebfa20752786127c3adca8438b
Maksim Melnikov <maksim.melnikov@arrival.com>
2021-06-15 22:56:52 +0300

    black box retry fix

--- lib/devices/black_box_meter_device.py
+++ lib/devices/black_box_meter_device.py
@@ -1,7 +1,11 @@
 import serial
 import time
 import re
+import logging
 
+logger = logging.getLogger('boreas')
+
+from lib.wrappers import retry
 
 class BlackBoxMeterDevice():
     def __init__(self, config=None):
@@ -15,6 +19,7 @@ class BlackBoxMeterDevice():
         self.conn.write(f"{value}\r".encode())
         time.sleep(0.25)
 
+    @retry(retries=3, time_between_retries=0.1, exception_class=Exception)
     def _read_value(self):
         """Read raw value from port, clean it up and convert to float."""
         raw_value = self.conn.read_all().decode()
@@ -23,5 +28,9 @@ class BlackBoxMeterDevice():
     
     def write_value_and_read_answer(self, value):
         self._write_value(value)
-        value = self._read_value()
-        return float(value) if value else None
+        try:
+            value = self._read_value()
+            return float(value) if value else None
+        except Exception as err:
+            logger.error(f"Cannot read answer after writing value {value}: {err}")
+            return None
