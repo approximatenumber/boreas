91eb1b6d62e0d515925b1b3bcedb2ee197049b40
Maksim Melnikov <maksim.melnikov@arrival.com>
2021-06-15 17:33:38 +0300

    parallelism

--- boreas.py
+++ boreas.py
@@ -1,7 +1,7 @@
 from time import sleep
 import logging
 import argparse
-from typing import Dict
+from typing import Dict, List
 import threading
 
 from lib.mqtt_publisher import MQTTPublisher
@@ -20,17 +20,65 @@ ENABLED_DEVICES = ['controller', 'wind_sensor', 'speed_meter', 'torque_meter', '
 logger = Logger.get_logger('boreas')
 
 
-def publish(publisher, device: str, topic_to_function: Dict[str, str]):
+publisher = MQTTPublisher()
+controller = Controller()
+wind_sensor = WindSensor()
+torque_meter = TorqueMeter()
+speed_meter = SpeedMeter()
+misc_devices = MiscDevices()
+
+dispath = {
+    'controller': {
+        'accum_solar_gen_energy': controller.get_accum_solar_gen_energy,
+        'accum_wind_gen_energy': controller.get_accum_wind_gen_energy,
+        'battery_voltage': controller.get_battery_voltage,
+        'daily_consump_energy': controller.get_daily_consump_energy,
+        'daily_gen_energy': controller.get_daily_gen_energy,
+        'daily_solar_gen_energy': controller.get_daily_solar_gen_energy,
+        'daily_wind_gen_energy': controller.get_daily_wind_gen_energy,
+        'load_1_current': controller.get_load_1_current,
+        'load_1_power': controller.get_load_1_power,
+        'load_2_current': controller.get_load_2_current,
+        'load_2_power': controller.get_load_2_power,
+        'pv_current': controller.get_pv_current,
+        'pv_power': controller.get_pv_power,
+        'pv_voltage': controller.get_pv_voltage,
+        'state': controller.get_state,
+        'total_consump_energy': controller.get_total_consump_energy,
+        'total_gen_energy': controller.get_total_gen_energy,
+        'wind_current': controller.get_wind_current,
+        'wind_power': controller.get_wind_power,
+        'wind_rotation_speed': controller.get_wind_rotation_speed,
+        'wind_voltage': controller.get_wind_voltage
+    },
+    'wind_sensor': {
+        'wind_power': wind_sensor.get_wind_power
+    },
+    'speed_meter': { 
+        'speed': speed_meter.get_speed
+    },
+    'torque_meter': {
+        'torque': torque_meter.get_peak
+    },
+    'misc': {
+        'cpu_temp': misc_devices.get_cpu_temp,
+        'board_temp': misc_devices.get_board_temp
+    }
+}
+
+
+def publish(devices: List[str]):
     while True:
-        logger.info(f"=> Reading device {device}")
-        for topic, function in topic_to_function.items():
-            value = function()
-            if not isinstance(value, (int, float)):
-                logger.error(f"===> Cannot get data from device \"{device}\" for topic \"{topic}\"")
-                # logger.error(f"Skipping further readings from \"{device}\" until next try")
-                continue
-            publisher.publish(topic=f"{device}/{topic}", value=value)
-            logger.info(f"===> Published: topic={device}/{topic}, value={value}")
+        for device in devices:
+            logger.info(f"=> Reading device {device}")
+            for topic, function in dispath[device].items():
+                value = function()
+                if not isinstance(value, (int, float)):
+                    logger.error(f"===> Cannot get data from device \"{device}\" for topic \"{topic}\"")
+                    # logger.error(f"Skipping further readings from \"{device}\" until next try")
+                    continue
+                publisher.publish(topic=f"{device}/{topic}", value=value)
+                logger.info(f"===> Published: topic={device}/{topic}, value={value}")
         sleep(PUBLISH_TIMEOUT)
 
 
@@ -43,56 +91,18 @@ def main():
     if args.debug:
         logger.setLevel(logging.DEBUG)
 
-    publisher = MQTTPublisher()
-    controller = Controller()
-    wind_sensor = WindSensor()
-    torque_meter = TorqueMeter()
-    speed_meter = SpeedMeter()
-    misc_devices = MiscDevices()
 
-    dispath = {
-        'controller': {
-            'accum_solar_gen_energy': controller.get_accum_solar_gen_energy,
-            'accum_wind_gen_energy': controller.get_accum_wind_gen_energy,
-            'battery_voltage': controller.get_battery_voltage,
-            'daily_consump_energy': controller.get_daily_consump_energy,
-            'daily_gen_energy': controller.get_daily_gen_energy,
-            'daily_solar_gen_energy': controller.get_daily_solar_gen_energy,
-            'daily_wind_gen_energy': controller.get_daily_wind_gen_energy,
-            'load_1_current': controller.get_load_1_current,
-            'load_1_power': controller.get_load_1_power,
-            'load_2_current': controller.get_load_2_current,
-            'load_2_power': controller.get_load_2_power,
-            'pv_current': controller.get_pv_current,
-            'pv_power': controller.get_pv_power,
-            'pv_voltage': controller.get_pv_voltage,
-            'state': controller.get_state,
-            'total_consump_energy': controller.get_total_consump_energy,
-            'total_gen_energy': controller.get_total_gen_energy,
-            'wind_current': controller.get_wind_current,
-            'wind_power': controller.get_wind_power,
-            'wind_rotation_speed': controller.get_wind_rotation_speed,
-            'wind_voltage': controller.get_wind_voltage
-        },
-        'wind_sensor': {
-            'wind_power': wind_sensor.get_wind_power
-        },
-        'speed_meter': { 
-            'speed': speed_meter.get_speed
-        },
-        'torque_meter': {
-            'torque': torque_meter.get_peak
-        },
-        'misc': {
-            'cpu_temp': misc_devices.get_cpu_temp,
-            'board_temp': misc_devices.get_board_temp
-        }
-    }
-
+    # first serial port
+    devices_port1 = ['controller', 'wind_sensor']
+    # second serial port
+    devices_port2 = ['torque_meter', 'speed_meter']
+    # misc devices
+    misc_devices = ['misc']
+    all_devices = [devices_port1, devices_port2, misc_devices]
 
     threads = []
-    for device, topics_to_functions in dispath.items():
-        thread = threading.Thread(target=publish, args=(publisher, device, topics_to_functions))
+    for devices_per_port in all_devices:
+        thread = threading.Thread(target=publish, args=(devices_per_port))
         thread.start()
         threads.append(thread)
 
