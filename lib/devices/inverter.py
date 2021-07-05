862dac574e0edfe5c10e6558fe5233c6edad191e
Maksim Melnikov <maksim.melnikov@arrival.com>
2021-07-05 19:09:15 +0300

    changes

--- lib/devices/inverter.py
+++ lib/devices/inverter.py
@@ -8,7 +8,7 @@ class Inverter():
 
     def __init__(self):
         self.config = InverterConfig()
-        self.device = serial.Serial(
+        self.serial = serial.Serial(
             port=self.config.PORT,
             baudrate=self.config.BAUDRATE,
             timeout=self.config.TIMEOUT
@@ -17,10 +17,10 @@ class Inverter():
     def _send_packet_and_get_answer(self, packet):
         for sent_byte in packet:
             self.serial.write(sent_byte)
-            received_byte = self.read(1)
+            received_byte = self.serial.read(len(sent_byte))
             if sent_byte != received_byte:
                 raise Exception(f"sent {sent_byte}, received {received_byte}")
-        answer = self.readall()
+        answer = self.serial.readall()
         return answer
 
     def get_pwr_consmp_from_net(self):
@@ -48,10 +48,24 @@ class InverterPacket():
 
     @property
     def packet(self):
+        """"""
         _packet = [self.start_symbol, self.page_size, *self.address, self.checksum, self.END_SYMBOL]
-        return [struct.pack('B', _byte) for _byte in _packet]
+        _bytes = []
+        for element in _packet:
+            # https://docs.python.org/3/library/struct.html#format-characters
+            if element <= 255:
+                size = 'B' # unsigned char
+            else:
+                size = 'H' # unsigned short
+            _byte = struct.pack(size, element)
+            _bytes.append(_byte)
+        return _bytes
 
     @property
     def checksum(self):
         checksum = 0x100 - (sum([self.READ_START_SYMBOL, self.page_size, *self.address]) % 256)
         return checksum
+
+
+inverer = Inverter()
+print(inverer.get_pwr_consmp_from_net())
