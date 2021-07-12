bfce1b15ec67facbcb81b93b17d1c039516e761f
Maksim Melnikov <maksim.melnikov@arrival.com>
2021-07-12 19:29:06 +0300

    changes

--- lib/devices/inverter.py
+++ lib/devices/inverter.py
@@ -88,12 +88,11 @@ class Inverter():
         return (_M_POWhourMAPCharge_HH * 65536 + _M_POWhourMAPCharge_H * 256 + _M_POWhourMAPCharge_L) / 100
 
     def get_net_current_sign(self):
-        _M_POWhourNET_sign_1 = self._read_value_from_device(page_size=0x03, address=self.config._M_POWhourNET_sign_1)
+        packet = self._read_value_from_device(page_size=0x03, address=self.config._M_POWhourNET_sign_1)
         # _M_POWhourNET_sign_2 = self._read_value_from_device(page_size=0x00, address=self.config._M_POWhourNET_sign_2)
         # _M_POWhourNET_sign_3 = self._read_value_from_device(page_size=0x00, address=self.config._M_POWhourNET_sign_3)
         # _M_POWhourNET_sign_4 = self._read_value_from_device(page_size=0x00, address=self.config._M_POWhourNET_sign_4)
-        return _M_POWhourNET_sign_1
-        return sign
+        return struct.unpack('i', packet[1:5])[0]
 
 
 class InverterPacket():
